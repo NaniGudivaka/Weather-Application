6416c2ee86ec29597decc853d7f4583c12735840
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-08-17 22:12:43 +0530

    Making application responsive

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -7,33 +7,46 @@ import HourlyForecast from './components/weather/HourlyForecast'
 import WeatherDetails from './components/weather/WeatherDetails'
 
 function App() {
-  
+
 
   return (
     <>
-    <div className='app-layout'>
-      <Sidebar />
+      <div className='app-layout'>
+        <Sidebar />
 
-      <div className='main-content'>
-        <Topbar />
+        <main className='main-content'>
+          <Topbar />
 
-       
-        <div className='dashboard'>
-          <CurrentWeather />
-          <WeatherDetails />
-          <HourlyForecast />
-        </div>
-        <div className='dev-text'>
-          <p>Application Is Actively Developing Please Wait For Some Time.....</p>
-        </div>
+
+          <div className='dashboard'>
+            <div className='current-weather-section'>
+
+              <CurrentWeather />
+            </div>
+
+            <div className='weather-details-section'>
+
+              <WeatherDetails />
+            </div>
+
+            <div className='hourly-forecast-section'>
+
+              <HourlyForecast />
+            </div>
+
+
+          </div>
+          <div className='dev-text'>
+            <p>Application Is Actively Developing Please Wait For Some Time.....</p>
+          </div>
+
+        </main>
+
 
       </div>
-      
 
-    </div>
-    
-    
-    
+
+
     </>
   )
 }
